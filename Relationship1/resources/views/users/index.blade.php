509512fea75aaaa2c3e4b96483089f04fcbb6d99
Nicolo <nicolorizzi1996@gmail.com>
2020-06-22 18:05:58 +0200

    print value

--- Relationship1/resources/views/users/index.blade.php
+++ Relationship1/resources/views/users/index.blade.php
@@ -4,8 +4,10 @@
         @foreach ($users as $user)
             <div class="user">
                 <h3>{{ $user->name }}</h3>
-                <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
-                @dump($user->info)
+                <img src="{{ $user->info['avatar'] }}" alt="{{ $user->name }}">
+                <p><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></p>
+                <div>{{ $user->info['address'] }}</div>
+                <div>{{ $user->info['phone'] }}</div>
             </div>
             @if (! $loop->last)
                     <hr>
