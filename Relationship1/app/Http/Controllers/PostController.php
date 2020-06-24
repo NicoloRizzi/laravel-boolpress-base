e16bda9b3c6c7b6f18579a3d06a412b378e9da2a
Nicolo <nicolorizzi1996@gmail.com>
2020-06-24 18:47:58 +0200

    show a new post view and orderby post archive

--- Relationship1/app/Http/Controllers/PostController.php
+++ Relationship1/app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
     public function index()
     {
         /* $posts = Post::all(); */
-        $posts = Post::Simplepaginate(5);
+        $posts = Post::orderBy('created_at', 'DESC')->paginate(5);
         return view('posts.index', compact('posts'));
     }
 
--- Relationship1/resources/views/posts/show.blade.php
+++ Relationship1/resources/views/posts/show.blade.php
@@ -2,4 +2,13 @@
     @section('main-content')
         <h1>{{ $post->title }}</h1>
         <p>{{ $post->body }}</p>
+
+        <section class="warap-tags mt-5">
+            <h4>Tags</h4>
+            @forelse ($post->tags as $tag)
+                <spa class="badge badge-primary badge-pill">{{ $tag->name}}</span>
+            @empty
+                <p>No actual tags for this post.</p>
+            @endforelse
+        </section>
     @endsection
