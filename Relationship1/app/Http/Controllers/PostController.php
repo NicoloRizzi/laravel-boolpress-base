77c2d68300e76a75558f836a9750f983e5843293
Nicolo <nicolorizzi1996@gmail.com>
2020-06-24 18:35:27 +0200

    show and create a new post

--- Relationship1/app/Http/Controllers/PostController.php
+++ Relationship1/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Tag;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -44,6 +45,27 @@ class PostController extends Controller
             'body' => 'required',
             'tags.*' => 'exists:tags,id'
         ]);
+
+        $data = $request->all();
+
+        //GET USER ID authentication fake
+        $data['user_id'] = 1;
+
+        //GENERATE POST slug
+        $data['slug'] = Str::slug($data['title'], '-');
+
+        //CREATE A NEW POST
+        $newPost = new Post();
+        $newPost->fill($data);
+        $saved = $newPost->save();
+
+        if($saved) {
+            if(!empty($data['tags'])) {
+                $newPost->tags()->attach($data['tags']);
+            
+                return redirect()->route('posts.show', $newPost->slug);
+            }
+        }
     }
 
     /**
@@ -56,6 +78,9 @@ class PostController extends Controller
     {
         //GET A POST
         $post = Post::where('slug', $slug)->first();
+        if (empty($post)) {
+            abort(404);
+        }
 
         return view('posts.show', compact('post'));
     }
